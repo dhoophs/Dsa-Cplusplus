208c224631a53aa1dfb4c2423ed7aed64cc5cfa7
dhoophs <160323336+dhoophs@users.noreply.github.com>
2024-07-25 19:58:57 +0530

    Add files via upload

---
+++ primeno.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+        if (n<=1){
+            return false;
+        }
+
+     for(int i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+}     
+
+int main(){
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    if(isPrime(n)){
+        cout<<"The no. is prime";
+    }
+    else{
+        cout<<"The no. is not prime";
+    }
+    }
+
+
